20ad390fcd31472f258c359dba2b179fa0567cd0
Taira Terashima <taira.815.terashima@gmail.com>
2020-01-29 07:03:34 +0900

    chore: Add TypeScript settings

---
+++ src/client/nofile.ts
@@ -0,0 +1,1 @@
+// TODO: File to be deleted.
---
+++ src/server/index.ts
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req: any, res: any) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening`));
