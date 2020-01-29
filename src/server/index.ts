900e390c1e0e830818be19c36f81a1e3523241e2
Taira Terashima <taira.815.terashima@gmail.com>
2020-01-29 09:42:33 +0900

    feat: Set compiling React

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-const staticFilesOptions = {
+const publicFilesOptions = {
     'dotfiles': 'ignore',
     'extensions': ['html'],
 }
-app.use(express.static('public', staticFilesOptions))
+app.use(express.static('public', publicFilesOptions));
+
+const jsFilesOptions = {
+  'dotfiles': 'ignore',
+  'extensions': ['js'],
+};
+app.use('/js', express.static('dist/client', jsFilesOptions));
 
 app.listen(port, () => console.log(`Example app listening`));
