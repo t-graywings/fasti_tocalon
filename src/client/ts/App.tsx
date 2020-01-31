2a62c4b328c55901daee15afab3bc977b820ffeb
Taira Terashima <taira.815.terashima@gmail.com>
2020-02-01 02:57:52 +0900

    feat: Add display image list

--- src/client/ts/App.tsx
+++ src/client/ts/App.tsx
@@ -1,29 +1,14 @@
 import * as React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
 
 import Header from "./common/Header";
 import Footer from "./common/Footer";
-import HostList from "./HostList";
+import Root from "./Root";
 
 const App = () => (
-  <BrowserRouter>
-    <Route exact path='/' component={Home} />
-    <Route path ='/about' component={About} />
-  </BrowserRouter>
-)
-
-const Home = () => (
   <>
-    <Header />
-    <HostList />
-    <Footer />
-  </>
-)
-
-const About = () => (
-  <>
-    <Header />
-    <Footer />
+  <Header />
+  <Root />
+  <Footer />
   </>
 )
 
